414a8687583fdcdfbd49ce0d2ae211f0f4ca9630
MaherKouki <koukimaher386@gmail.com>
2025-02-05 22:25:08 +0100

    first commit

---
+++ Keycloak-auth-service/Keycloak-auth-service/src/test/java/Keycloak_Auth_Service/Keycloak_auth_service/KeycloakAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Keycloak_Auth_Service.Keycloak_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
