0c42031ad27b80a36cf3da5f2cc1aeb3f0ae6f12
MaherKouki <koukimaher386@gmail.com>
2025-02-20 00:30:16 +0100

    get roles of a user

---
+++ KeycloakManaging-ui/src/app/utils/keycloak/keycloak.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import Keycloak from 'keycloak-js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KeycloakService {
+
+  private _keycloak: Keycloak | undefined;
+
+  constructor() { }
+
+  get keycloak(){
+    if (!this._keycloak) {
+      this._keycloak = new Keycloak({
+        url: 'http://localhost:9082',
+        realm : 'spring',
+        clientId : 'admin-cli'
+      });
+    }
+    return this._keycloak;
+  }
+
+
+  async init(){
+    const authenticated = await this.keycloak.init({
+      onLoad: 'login-required'
+    })
+  }
+
+  async login(){
+    await this.keycloak.login();
+  }
+
+  get userInfo() : string{
+    return this.keycloak?.tokenParsed?.sub as string;
+  }
+
+  get isTokenValid() : boolean{
+    return !this.keycloak.isTokenExpired();
+  }
+
+  get fullName() : string{
+    return this.keycloak?.tokenParsed?.['name'] as string;
+  }
+
+  logout(){
+    return this.keycloak.logout({redirectUri: 'http://localhost:4200'});
+  }
+
+  accountManagement(){
+    return this.keycloak.accountManagement();
+  }
+
+
+
+
+
+
+
+
+}
