b3546b9806047149f493bd1e3f6b53e8af8aecf0
MaherKouki <koukimaher386@gmail.com>
2025-02-21 00:36:26 +0100

    get roles of a user

--- KeycloakManaging-ui/src/app/app.config.ts
+++ KeycloakManaging-ui/src/app/app.config.ts
@@ -15,8 +15,6 @@ export function kcFactory(kcService: KeycloakService) {
   return () => kcService.init();
 }
 
-
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
