9d281b9c3838c85c7882a846e96bafcd0cf73c4f
MaherKouki <koukimaher386@gmail.com>
2025-02-23 01:41:49 +0100

    getting roles is done

--- Keycloak-auth-service/Keycloak-auth-service/src/main/java/Keycloak_Auth_Service/Keycloak_auth_service/config/WebSecurityConfig.java
+++ Keycloak-auth-service/Keycloak-auth-service/src/main/java/Keycloak_Auth_Service/Keycloak_auth_service/config/WebSecurityConfig.java
@@ -25,8 +25,6 @@ public class WebSecurityConfig {
                     "/public/**"
             );
 
-
-
             web.ignoring().requestMatchers(
                     HttpMethod.DELETE,
                     "/public/**",
